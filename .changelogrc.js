317ebb4248b191fbde6b499fa51d26211c7236e7
wakaka378 <gebilaofl@163.com>
2023-08-21 15:28:18 +0800

    :rocket: release(accounts): release 111
    re324

--- .changelogrc.js
+++ .changelogrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  displayTypes: ['feat', 'fix', ':rocket: release', 'chore'], // 默认显示的type
+  displayTypes: ['feat', 'fix', ':rocket: release', 'ci'], // 默认显示的type
 };
--- .versionrc.js
+++ .versionrc.js
@@ -1,19 +1,19 @@
 module.exports = {
   header: '# 更新历史 \n\n',
-  "types": [
-    { "type": "feat", "section": "✨ Features | 新功能" },
-    { "type": "release", "section": "✨ Release | 新版本" },
-    { "type": "fix", "section": "🐛 Bug Fixes | Bug 修复" },
-    { "type": "init", "section": "🎉 Init | 初始化" },
-    { "type": "docs", "section": "✏️ Documentation | 文档" },
-    { "type": "style", "section": "💄 Styles | 风格" },
-    { "type": "refactor", "section": "♻️ Code Refactoring | 代码重构" },
-    { "type": "perf", "section": "⚡ Performance Improvements | 性能优化" },
-    { "type": "test", "section": "✅ Tests | 测试" },
-    { "type": "revert", "section": "⏪ Revert | 回退" },
-    { "type": "build", "section": "📦‍ Build System | 打包构建" },
-    { "type": "chore", "section": "🚀 Chore | 构建/工程依赖/工具" },
-    { "type": "ci", "section": "👷 Continuous Integration | CI 配置" }
-  ]
-  
+  types: [
+    { type: 'feat', section: '✨ Features | 新功能', hidden: false},
+    { type: 'release', section: '🚀 Release | 发布', hidden: false},
+    { type: 'fix', section: '🐛 Bug Fixes | Bug 修复', hidden: false },
+    { type: 'init', section: '🎉 Init | 初始化', hidden: true },
+    { type: 'test', section: '✅ Tests | 测试', hidden: false },
+    { type: 'revert', section: '⏪ Revert | 回退', hidden: false },
+    { type: 'build', section: '📦‍ Build System | 打包构建', hidden: false },
+    { type: 'ci', section: 'ci | 打包构建fsadfa', hidden: false },
+  ],
+  skip: {
+    bump: true, //缓存变化，并重置git状态至最近的tag节点
+    changelog: false, //自动产出changelog文档
+    commit: true, //提交变动
+    tag: true //在git中增加tag标识
+  }
 }
