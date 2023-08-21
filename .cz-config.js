8c97a63bf110ee92712aca22a5d66afa38406009
wakaka378 <gebilaofl@163.com>
2023-08-21 11:18:02 +0800

    WIP: wipta
    wett

--- .cz-config.js
+++ .cz-config.js
@@ -1,76 +1,50 @@
-'use strict';
-
 module.exports = {
   types: [
+    { value: 'feat', name: 'feat:     A new feature' },
+    { value: 'fix', name: 'fix:      A bug fix' },
+    { value: 'docs', name: 'docs:     Documentation only changes' },
     {
-      value: ':rocket: release',
-      name: '🚀  release:  发布版本/发布标签',
+      value: 'style',
+      name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
     },
     {
-      value: ':construction: WIP',
-      name: '💪  WIP:      正在进行的工作',
+      value: 'refactor',
+      name: 'refactor: A code change that neither fixes a bug nor adds a feature',
     },
     {
-      value: ':sparkles: feat',
-      name: '✨  feat:     新的特性',
+      value: 'perf',
+      name: 'perf:     A code change that improves performance',
     },
+    { value: 'test', name: 'test:     Adding missing tests' },
     {
-      value: ':bug: fix',
-      name: '🐛  fix:      修复Bug',
+      value: 'chore',
+      name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
     },
-    {
-      value: ':wrench: CI',
-      name: '🔧  CI:       目录结构变更(CI, Building, Tool...)',
-    },
-
-    {
-      value: ':pencil: docs',
-      name: '📝  docs:     仅文档更改',
-    },
-    {
-      value: ':twisted_rightwards_arrows: merge',
-      name: '🔀  merge:    合并分支',
-    },
-    {
-      value: ':rewind: revert',
-      name: '⏪  revert:   版本回滚',
-    },
-    {
-      value: ':white_check_mark: test',
-      name: '✅  test:     添加缺失的测试或更正现有测试',
-    },
-    {
-      value: ':chart_with_upwards_trend: perf',
-      name: '📈  perf:     提高性能的代码更改',
-    },
-    {
-      value: ':thought_balloon: chore',
-      name: '🗯   chore:    不修改src或测试文件的更改。例如更新构建任务、包管理器',
-    },
-    {
-      value: ':lipstick: ui',
-      name: '💄  UI:       更新UI和样式文件。',
-    },
-    {
-      value: ':art: style',
-      name: '🎨  style:    不影响代码含义的更改（空白、格式、缺少分号等）',
-    },
-    {
-      value: ':package: dep_up',
-      name: '📦  dep_up:   更新已编译的文件或包。',
-    },
-
-    {
-      value: ':hammer: refactor',
-      name: '🔨  refactor: 既不修复错误也不添加功能的代码更改',
-    },
-    {
-      value: ':truck: mv',
-      name: '🚚  mv:       移动或重命名文件。',
-    },
+    { value: 'revert', name: 'revert:   Revert to a commit' },
+    { value: 'WIP', name: 'WIP:      Work in progress' },
   ],
 
-  scopes: [],
+  scopes: [{ name: 'accounts' }, { name: 'admin' }, { name: 'exampleScope' }, { name: 'changeMe' }],
+
+  usePreparedCommit: false, // to re-use commit from ./.git/COMMIT_EDITMSG
+  allowTicketNumber: false,
+  isTicketNumberRequired: false,
+  ticketNumberPrefix: 'TICKET-',
+  ticketNumberRegExp: '\\d{1,5}',
+
+  // it needs to match the value for field type. Eg.: 'fix'
+  /*
+  scopeOverrides: {
+    fix: [
+
+      {name: 'merge'},
+      {name: 'style'},
+      {name: 'e2eTest'},
+      {name: 'unitTest'}
+    ]
+  },
+  */
+  // override the messages, defaults are as follows
   messages: {
     type: '选择一种你的提交类型:',
     scope: '选择一个scope (可选):',
@@ -82,6 +56,15 @@ module.exports = {
     footer: '关联关闭的issue，例如：#31, #34(可选):\n',
     confirmCommit: '确定提交说明? yes/no',
   },
+
   allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix'],
+  // skip any questions you want
+  // skipQuestions: ['scope', 'body'],
+
+  // limit subject length
+  subjectLimit: 100,
+  // breaklineChar: '|', // It is supported for fields body and footer.
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true, // default is false
 };
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,14 @@
-module.exports = { extends: ['cz'] }
+module.exports = {
+  extends: ['@commitlint/config-conventional']
+  // rules: {
+  //   'type-enum': [
+  //     2,
+  //     'always',
+  //     ['WIP', 'feat', 'fix', 'refactor', 'docs', 'test', 'style', 'chore', 'revert']
+  //   ],
+  //   'type-case': [2, 'always', ['lower-case', 'upper-case']],
+  //   'scope-case': [0, 'never'],
+  //   'subject-case': [0, 'never'],
+  //   'scope-empty': [2, 'never']
+  // }
+}
